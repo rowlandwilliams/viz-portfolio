d8b187dbd389152a2e31356ae5243c08541964f4
rowlandwilliams <rowland.williams.94@gmail.com>
2021-08-17 16:50:33 +0100

    add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { NavBar } from "./components/Navbar/NavBar";
 import { Projects } from "./components/Projects/Projects";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
