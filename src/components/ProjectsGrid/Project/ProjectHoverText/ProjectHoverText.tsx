68b0eec3f9103e9abf649f8b0d427123cbac3b10
rowlandwilliams <rowland.williams.94@gmail.com>
2021-08-19 16:51:19 +0100

    add small hover text on mob

--- src/components/ProjectsGrid/Project/ProjectHoverText/ProjectHoverText.tsx
+++ src/components/ProjectsGrid/Project/ProjectHoverText/ProjectHoverText.tsx
@@ -11,7 +11,7 @@ export const ProjectHoverText = ({
 }: Props) => {
   return (
     <div
-      className={`absolute top-0 w-full h-full text-center text-opacity-0 hover:text-opacity-100 bg-${backgroundColor} transition-bg-opacity duration-200 bg-opacity-0 hover:bg-opacity-80 text-white`}
+      className={`absolute top-0 w-full h-full text-center text-xs md:text-base text-opacity-0 hover:text-opacity-100 bg-${backgroundColor} transition-bg-opacity duration-200 bg-opacity-0 hover:bg-opacity-80 text-white`}
     >
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl">
         {projectTitle}
