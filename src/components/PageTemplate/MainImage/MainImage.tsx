79cd20494f91478a0a516c624f4be28fc71d1d0d
rowlandwilliams <rowland.williams.94@gmail.com>
2021-08-20 19:12:50 +0100

    fix main image width

--- src/components/PageTemplate/MainImage/MainImage.tsx
+++ src/components/PageTemplate/MainImage/MainImage.tsx
@@ -8,7 +8,7 @@ export const MainImage = ({ projectUrl, mainImgSrc, imgName }: Props) => {
   return (
     <a href={projectUrl} target="_blank" rel="noopener noreferrer">
       <div className="flex justify-center md:h-144 transition-opacity duration-700">
-        <img src={mainImgSrc} alt={imgName} className="w-full max-w-3xl py-4" />
+        <img src={mainImgSrc} alt={imgName} className="w-full py-4" />
       </div>
     </a>
   );
