a0df9058d2cc3896e7edba2a07360fddd9437542
rowlandwilliams <rowland.williams.94@gmail.com>
2021-09-01 16:26:41 +0100

    fix hover text

--- src/components/AnimatedRoutes/ProjectsGrid/ProjectColumns/Project/ProjectHoverText/ProjectHoverText.tsx
+++ src/components/AnimatedRoutes/ProjectsGrid/ProjectColumns/Project/ProjectHoverText/ProjectHoverText.tsx
@@ -6,9 +6,9 @@ interface Props {
 export const ProjectHoverText = ({ projectTitle, backgroundColor }: Props) => {
   return (
     <div
-      className={`absolute top-0 w-full h-full text-center text-xs md:text-base text-opacity-0 hover:text-opacity-100 bg-${backgroundColor} transition-bg-opacity duration-200 bg-opacity-0 hover:bg-opacity-80 text-white`}
+      className={`absolute top-0 w-full h-full text-center  text-opacity-0 hover:text-opacity-100 bg-${backgroundColor} transition-bg-opacity duration-200 bg-opacity-0 hover:bg-opacity-80 text-white`}
     >
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs md:text-xl">
         {projectTitle}
       </div>
     </div>
